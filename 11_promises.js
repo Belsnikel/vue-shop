e7b8d4f3046725e0c65790ba0d76c6fda02b66ca
Belsnikel <35655526+Belsnikel@users.noreply.github.com>
2024-02-02 22:11:43 +0300

    Update 11_promises.js

--- 11_promises.js
+++ 11_promises.js
@@ -1,29 +1,29 @@
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('Success')
-//   }, 500)
-// })
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Success')
+  }, 500)
+})
 
 const delay = ms => new Promise(((resolve, reject) => {
   setTimeout(() => resolve(`Done! ${ms}`), ms)
 }))
 
-// delay(1000)
-//   .then(data => delay(500))
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err))
-//   .finally(() => console.log('Finally'))
+delay(1000)
+  .then(data => delay(500))
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+  .finally(() => console.log('Finally'))
 
-// async function asyncDelay() {
-//   try {
-//     const data = await delay(2000)
-//     console.log(data)
-//   } catch (e) {
-//     console.log('Error', e)
-//   }
-// }
-//
-// asyncDelay()
+async function asyncDelay() {
+  try {
+    const data = await delay(2000)
+    console.log(data)
+  } catch (e) {
+    console.log('Error', e)
+  }
+}
+
+asyncDelay()
 
 Promise.all([
   delay(1000),
@@ -36,3 +36,24 @@ Promise.race([
   delay(500),
   delay(2000)
 ]).then(data => console.log(data))
+
+
+
+  
+<button id = "loadFile">Load</button>
+<div id = "result"></div>
+function loadFile(url) {
+  return new Promise((resolve) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.send()
+    xhr.addEventListener('load', () => {
+      result.innerText = xhr.responseText
+      resolve()
+    })
+  })
+}
+
+const result = document.getElementById('result')
+const loadFile = document.querySelector('#loadFile')
+
