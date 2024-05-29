343aaa53feaca16725fbeb1c2851c9a8c26aaa20
Belsnikel <afterouy@gmail.com>
2024-05-29 13:44:34 +0300

    changed auth  form

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,21 +1,21 @@
 <script setup>
-import {ref} from 'vue';
-import {useAuthStore} from '../stores/auth';
-import {useRouter} from 'vue-router'
+import { ref } from 'vue'
+import { useAuthStore } from '../stores/auth'
+import { useRouter } from 'vue-router'
 
-import InputText from 'primevue/inputtext';
-import Button from 'primevue/button';
-import Message from 'primevue/message';
+import InputText from 'primevue/inputtext'
+import Button from 'primevue/button'
+import Message from 'primevue/message'
 import Loader from '../components/Loader.vue'
 
-const authStore = useAuthStore();
-const router = useRouter();
+const authStore = useAuthStore()
+const router = useRouter()
 
-const email = ref();
-const password = ref();
+const email = ref()
+const password = ref()
 
 const signin = async () => {
-  await authStore.auth({email: email.value, password: password.value}, 'signin')
+  await authStore.auth({ email: email.value, password: password.value }, 'signin')
   router.push('/cars')
 }
 </script>
@@ -26,19 +26,19 @@ const signin = async () => {
     <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
     <div class="p-inputgroup flex-1">
       <span class="p-inputgroup-addon">
-          <i class="pi pi-user"></i>
+        <i class="pi pi-user"></i>
       </span>
       <InputText type="email" v-model="email" placeholder="Your Email" />
     </div>
     <div class="p-inputgroup flex-1">
       <span class="p-inputgroup-addon">
-          <i class="pi pi-at"></i>
+        <i class="pi pi-at"></i>
       </span>
       <InputText type="password" v-model="password" placeholder="Password" />
     </div>
-    <Loader v-if="authStore.loader"/>
+    <Loader v-if="authStore.loader" />
     <div v-else class="flex flex-column gap-3">
-      <Button label="Signin" @click="signin"/>
+      <Button label="Signin" @click="signin" />
       <span>Are you not registered yet? <router-link to="/signup">Sign up</router-link></span>
     </div>
   </form>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -24,31 +24,6 @@ const signup = async () => {
   router.push('/cars')
 }
 </script>
-
-<!-- <template>
-  <h2>Sign Up</h2>
-  <form class="flex flex-column gap-3">
-    <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon">
-          <i class="pi pi-user"></i>
-      </span>
-      <InputText type="email" v-model="email" placeholder="Your Email" />
-    </div>
-    <div class="p-inputgroup flex-1">
-      <span class="p-inputgroup-addon">
-          <i class="pi pi-at"></i>
-      </span>
-      <InputText type="password" v-model="password" placeholder="Password" />
-    </div>
-    <Loader v-if="authStore.loader"/>
-    <div v-else class="flex flex-column gap-3">
-      <Button label="Signup" @click="signup"/>
-      <span>Are you already registered? <router-link to="/signin">Sign in</router-link></span>
-    </div>
-  </form>
-</template> -->
-
 <template>
   <div class="fixed top-0 left-0 h-full w-full z-10">
     <div
@@ -66,7 +41,7 @@ const signup = async () => {
         &#x2715;
       </div>
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form @submit="() => emit('closeAuthForm')" class="space-y-6" action="#" method="POST">
+        <form @submit="() => emit('closeAuthForm')" class="space-y-6" method="POST">
           <Message v-if="authStore.error" severity="warn">{{ authStore.error }}</Message>
           <div>
             <label for="email" class="block text-lg font-medium leading-6 text-gray-900"
@@ -171,31 +146,22 @@ const signup = async () => {
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
 }
 
-/* стили при наведении курсора на checkbox */
 .custom-checkbox:not(:disabled):not(:checked) + label:hover::before {
   border-color: #b3d7ff;
 }
-/* стили для активного состояния чекбокса (при нажатии на него) */
 .custom-checkbox:not(:disabled):active + label::before {
   background-color: #b3d7ff;
   border-color: #b3d7ff;
 }
-/* стили для чекбокса, находящегося в фокусе */
+
 .custom-checkbox:focus + label::before {
   box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.25);
 }
-/* стили для чекбокса, находящегося в фокусе и не находящегося в состоянии checked */
+
 .custom-checkbox:focus:not(:checked) + label::before {
   border-color: #80bdff;
 }
-/* стили для чекбокса, находящегося в состоянии disabled */
 .custom-checkbox:disabled + label::before {
   background-color: #e9ecef;
 }
 </style>
-
-<!-- 
-  сделать проверку:
-  если логин или пароль есть в localStorage, то открывается страница пользователя
-  добавить показать пароль и валидацию минимальной длинны пароля
- -->
